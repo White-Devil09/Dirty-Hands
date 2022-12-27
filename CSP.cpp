119ed5cb597fd1644a07779be6899842585ee00b
White-Devil <bhanujarpula9@gmail.com>
2022-12-27 12:09:33 +0530

    added atomic variable

--- CSP.cpp
+++ CSP.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <thread>
-#include <unistd.h>
+#include <atomic> 
 
 using namespace std;
 
 long long count = 0;
+atomic <long long> count_atm(0);
 
 void Task(){
-	for(int i = 0 ; i < 1000000 ; i++) count++;
+	for(int i = 0 ; i < 1000000 ; i++){
+		count++;
+		count_atm++;
+	}
 }
 
 int main(){
@@ -17,7 +21,10 @@ int main(){
 	t1.join();
 	t2.join();
 
+	//This is normal variable 
 	cout<<count<<endl;
+	//This is atomic variable i.e., context switching happens only when manipulation is completed
+	cout<<count_atm<<endl;
 
 	return 0;
 }
